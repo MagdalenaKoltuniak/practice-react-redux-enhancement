409cfe8cdcb211e200352ca3d3cb7e1205f0a4fc
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-09-21 15:46:59 +0200

    add task 2

--- 02/Task02.js
+++ 02/Task02.js
@@ -5,11 +5,13 @@ import withPopup from './../src/hoc/withPopup';
 const ComponentWithPopup = withPopup(Welcome);
 
 const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+	<section>
+		<h2>Task 02</h2>
+		<ComponentWithPopup
+			overlayStyle={{ background: 'rgba(255, 0, 0, 0.3)' }}
+			popupStyle={{ background: 'yellow', opacity: 0.8 }}
+		/>
+	</section>
+);
 
 export default Task02;
-
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,64 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = WrappedComponent => {
+	return class extends React.Component {
+		state = {
+			isOpen: true,
+		};
 
+		closePopup = () => {
+			this.setState({ isOpen: false });
+		};
+
+		render() {
+			const { isOpen } = this.state;
+
+			const { 
+                overlayStyle = {}, 
+                popupStyle = {} 
+            } = this.props;
+
+			if (!isOpen) return <WrappedComponent {...this.props} />;
+
+			return (
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						position: 'fixed',
+						top: 0,
+						left: 0,
+						width: '100vw',
+						height: '100vh',
+						background: 'rgba(0,0,0,0.5)',
+                        ...overlayStyle,
+					}}>
+					<div
+						style={{
+							background: 'white',
+							padding: '20px',
+							borderRadius: '8px',
+							position: 'relative',
+                            ...popupStyle,
+						}}>
+                            
+						<button
+							style={{
+								position: 'absolute',
+								top: '10px',
+								right: '10px',
+							}}
+							onClick={this.closePopup}>
+							&times;
+						</button>
+
+						<WrappedComponent {...this.props} />
+					</div>
+				</div>
+			);
+		}
+	};
+};
 
 export default withPopup;
